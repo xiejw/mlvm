faff94b94055645a80ffb4a3faaca5fae055c0d1
Jianwei Xie <xiejw0217@gmail.com>
2019-09-29 11:58:22 -0700

    Added kind.

--- mlvm/lib/Comp/Tensor.h
+++ mlvm/lib/Comp/Tensor.h
@@ -5,10 +5,18 @@ namespace mlvm {
 namespace comp {
 
 class Tensor {
+ public:
+  enum class Kind { Constant };
+
+  Kind kind() const { return kind_; }
+
  private:
   friend std::ostream& operator<<(std::ostream& os, const Tensor& arr) {
     return os;
   }
+
+ private:
+  Kind kind_;
 };
 
 }  // namespace comp
