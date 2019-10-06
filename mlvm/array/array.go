31ec1798e65f89d0095d4c83a3b9e6ea4dcd1593
Jianwei Xie <xiejw0217@gmail.com>
2019-10-05 18:20:19 -0700

    Improved array and array new.

--- mlvm/array/array.go
+++ mlvm/array/array.go
@@ -1,15 +1,26 @@
 package array
 
+import (
+	"fmt"
+)
+
+var (
+	ErrEmptyShape     = fmt.Errorf("Shape must be non-empty (rank >= 1).")
+	ErrNonPositiveDim = fmt.Errorf("Dimension must be positive.")
+
+	ErrEmptyData = fmt.Errorf("Data must be non-empty.")
+)
+
 type Dimension int
+
+type Shape struct {
+	dims []Dimension
+}
 
 type Float float32
 
-type Shape struct {
-	Dims []Dimension
-}
-
 type Data struct {
-	Value []Float
+	value []Float
 }
 
 type Array struct {
@@ -30,14 +41,35 @@ func (arr *Array) Data() *Data {
 	return arr.data
 }
 
-// func IsScalar(shape *Shape) bool {
-// 	return len(shape) == 1 && shape[0] == 1
-// }
-//
-// func ElementCount(shape []Dimension) int {
-// 	count := 1
-// 	for _, dim := range shape {
-// 		count *= int(dim)
-// 	}
-// 	return count
-// }
+func (shape *Shape) Validate() (int, error) {
+	if len(shape.dims) == 0 {
+		return 0, ErrEmptyShape
+	}
+	count := 1
+	for _, dim := range shape.dims {
+		if int(dim) <= 0 {
+			return 0, ErrNonPositiveDim
+		}
+		count *= int(dim)
+	}
+	return count, nil
+}
+
+func (shape *Shape) IsScalar() bool {
+	return len(shape.dims) == 1 && shape.dims[0] == 1
+}
+
+func (shape *Shape) ElementCount() int {
+	count := 1
+	for _, dim := range shape.dims {
+		count *= int(dim)
+	}
+	return count
+}
+
+func (data *Data) Validate() (int, error) {
+	if data.value == nil {
+		return 0, ErrEmptyData
+	}
+	return len(data.value), nil
+}
--- mlvm/array/array_new.go
+++ mlvm/array/array_new.go
@@ -1,29 +1,36 @@
 package array
 
 import (
-	_ "fmt"
+	"fmt"
 )
 
 // Returns an Array for the data with given shape.
 //
 // All data passed in should not be not mutated in future.
 func NewArray(name string, dims []Dimension, value []Float) (*Array, error) {
-	// // Check shape and dimensions.
-	// eleCount := Dimension(1)
-	// for _, dim := range shape {
-	// 	if dim < 1 {
-	// 		return nil, fmt.Errorf("Tensor (%v) should have all positive dimension, but got %v", name, dim)
-	// 	}
-	// 	eleCount *= dim
-	// }
-	// if int(eleCount) != len(value) {
-	// 	return nil, fmt.Errorf("Tensor (%v) should have %v elements, but got %v", name, eleCount, len(value))
-	// }
+
+	shape := &Shape{dims: dims}
+	eleCount, err := shape.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	data := &Data{value: value}
+	valueCount, err := data.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	if eleCount != valueCount {
+		return nil, fmt.Errorf(
+			"Array (%v) should have %v elements, but got %v",
+			name, eleCount, valueCount)
+	}
 
 	// Now construct.
 	return &Array{
 		name:  name,
-		shape: &Shape{Dims: dims},
-		data:  &Data{Value: value},
+		shape: shape,
+		data:  data,
 	}, nil
 }
