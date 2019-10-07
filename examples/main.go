098838ab36b5af4dbf72e830449a82d5e23eab44
Jianwei Xie <xiejw0217@gmail.com>
2019-10-06 19:41:38 -0700

    Added example.

--- examples/main.go
+++ examples/main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 
 	"github.com/xiejw/mlvm/mlvm/array"
-	_ "github.com/xiejw/mlvm/mlvm/compile/ast"
+	"github.com/xiejw/mlvm/mlvm/compile/ast"
 )
 
 func main() {
-	arr := array.NewArrayOrDie("a", []array.Dimension{2, 1}, []array.Float{1.0, 2.0})
-	fmt.Printf("%v", arr)
+	a := array.NewArrayOrDie("a", []array.Dimension{2, 1}, []array.Float{1.0, 2.0})
+	b := array.NewArrayOrDie("b", []array.Dimension{2, 1}, []array.Float{2.1, 3.2})
+	fmt.Printf("Array %v: %v", a.Name(), a)
+	fmt.Printf("Array %v: %v", b.Name(), b)
+
+	m := ast.NewModule()
+	ta := m.NewConstant(a)
+	tb := m.NewConstant(b)
+	fmt.Printf("Tensor %v: %v\n", ta.Name(), ta)
+	fmt.Printf("Tensor %v: %v\n", tb.Name(), tb)
 }
