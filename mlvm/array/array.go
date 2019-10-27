f3357e9296139ad49658f22e7a4c411209a8e0ff
Jianwei Xie <xiejw0217@gmail.com>
2019-10-27 09:27:57 -0700

    Split the shape out.

--- mlvm/array/array.go
+++ mlvm/array/array.go
@@ -5,19 +5,9 @@ import (
 )
 
 var (
-	// Shape related errors.
-	ErrEmptyShape     = fmt.Errorf("Shape must be non-empty (rank >= 1).")
-	ErrNonPositiveDim = fmt.Errorf("Dimension must be positive.")
-
 	// Data related errors.
 	ErrEmptyData = fmt.Errorf("Data must be non-empty.")
 )
-
-type Dimension int
-
-type Shape struct {
-	dims []Dimension
-}
 
 type Float float32
 
@@ -41,40 +31,6 @@ func (arr *Array) Shape() *Shape {
 
 func (arr *Array) Data() *Data {
 	return arr.data
-}
-
-func (shape *Shape) Validate() (int, error) {
-	if len(shape.dims) == 0 {
-		return 0, ErrEmptyShape
-	}
-	count := 1
-	for _, dim := range shape.dims {
-		if int(dim) <= 0 {
-			return 0, ErrNonPositiveDim
-		}
-		count *= int(dim)
-	}
-	return count, nil
-}
-
-func (shape *Shape) IsScalar() bool {
-	return len(shape.dims) == 1 && shape.dims[0] == 1
-}
-
-func (shape *Shape) Rank() int {
-	return len(shape.dims)
-}
-
-func (shape *Shape) Dims() []Dimension {
-	return shape.dims
-}
-
-func (shape *Shape) ElementCount() int {
-	count := 1
-	for _, dim := range shape.dims {
-		count *= int(dim)
-	}
-	return count
 }
 
 func (data *Data) Validate() (int, error) {
---
+++ mlvm/array/shape.go
@@ -0,0 +1,51 @@
+package array
+
+import (
+	"fmt"
+)
+
+var (
+	// Shape related errors.
+	ErrEmptyShape     = fmt.Errorf("Shape must be non-empty (rank >= 1).")
+	ErrNonPositiveDim = fmt.Errorf("Dimension must be positive.")
+)
+
+type Dimension int
+
+type Shape struct {
+	dims []Dimension
+}
+
+func (shape *Shape) Validate() (int, error) {
+	if len(shape.dims) == 0 {
+		return 0, ErrEmptyShape
+	}
+	count := 1
+	for _, dim := range shape.dims {
+		if int(dim) <= 0 {
+			return 0, ErrNonPositiveDim
+		}
+		count *= int(dim)
+	}
+	return count, nil
+}
+
+func (shape *Shape) IsScalar() bool {
+	return len(shape.dims) == 1 && shape.dims[0] == 1
+}
+
+func (shape *Shape) Rank() int {
+	return len(shape.dims)
+}
+
+func (shape *Shape) Dims() []Dimension {
+	return shape.dims
+}
+
+func (shape *Shape) ElementCount() int {
+	count := 1
+	for _, dim := range shape.dims {
+		count *= int(dim)
+	}
+	return count
+}
