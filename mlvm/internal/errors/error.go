522fdd449dc468ec2d8fae6ae507812b5f55e9b6
Jianwei Xie <xiejw0217+xiejw2g@gmail.com>
2019-10-30 10:46:33 -0700

    Added test for error.

--- mlvm/internal/errors/error.go
+++ mlvm/internal/errors/error.go
@@ -5,15 +5,36 @@ import (
 	"fmt"
 )
 
+// Wrapper of fmt.Errorf.
+func Errorf(msg string, args ...interface{}) error {
+	return fmt.Errorf(msg, args...)
+}
+
+// Wrapper of fmt.Errorf with a root cause `original`.
+func ErrorfW(original error, msg string, args ...interface{}) error {
+	ierr := &internalError{
+		Message:  fmt.Sprintf(msg, args...),
+		Original: original,
+	}
+	return ierr
+}
+
 type internalError struct {
 	Message  string
 	Original error
 }
 
+// Format the nested error.
+//
+// Example output:
+//
+//     Unexpected error during creating instruction for opAdd_001
+//       \-> Fail to infer result shapes for Op kind `opAdd`
+//         \-> Expected two operands, got: 1
 func (ierr *internalError) Error() string {
 	buf := new(bytes.Buffer)
-
 	buf.WriteString(ierr.Message)
+
 	var err error
 	err = ierr.Original
 	indent := 0
@@ -21,6 +42,8 @@ func (ierr *internalError) Error() string {
 		if err == nil {
 			break
 		}
+
+		// Formats the indent.
 		buf.WriteString("\n  ")
 		for i := 0; i < indent; i++ {
 			buf.WriteString("  ")
@@ -39,18 +62,4 @@ func (ierr *internalError) Error() string {
 	}
 
 	return buf.String()
-}
-
-// Wrapper of fmt.Errorf.
-func Errorf(msg string, args ...interface{}) error {
-	return fmt.Errorf(msg, args...)
-}
-
-// Wrapper of fmt.Errorf with a root cause.
-func ErrorfW(original error, msg string, args ...interface{}) error {
-	ierr := &internalError{
-		Message:  fmt.Sprintf(msg, args...),
-		Original: original,
-	}
-	return ierr
 }
--- mlvm/internal/errors/error_test.go
+++ mlvm/internal/errors/error_test.go
@@ -10,3 +10,14 @@ func TestErrorf(t *testing.T) {
 		t.Errorf("Unexpected error, got %v", err)
 	}
 }
+
+func TestErrorfW(t *testing.T) {
+	err1 := Errorf("a")
+	err2 := ErrorfW(err1, "b")
+	err3 := ErrorfW(err2, "c")
+	if err3.Error() != `c
+  \-> b
+    \-> a` {
+		t.Errorf("Unexpected error, got %v", err3)
+	}
+}
