bc34781995f0d74619c6e49b71947cd251ef6b43
Jianwei Xie <xiejw0217@gmail.com>
2019-12-30 21:17:34 -0800

    Added more test fields for TensorLike array.

--- mlvm/Computation/TensorLikeTest.cpp
+++ mlvm/Computation/TensorLikeTest.cpp
@@ -18,6 +18,15 @@ TEST_F(TensorLikeTest, CheckArrayTensor) {
   Function fn{"test"};
   auto tensor_or = fn.makeTensor({{1, 2, 3, 4, 5}, {5, 1}});
   ASSERT_TRUE(tensor_or.ok());
+
+  auto tensor = tensor_or.consumeValue();
+  ASSERT_STREQ("%c_0", tensor->name().c_str());
+  ASSERT_EQ(TensorLike::Type::Constant, tensor->type());
+  ASSERT_STREQ("<5, 1>", tensor->shape().string().c_str());
+  ASSERT_EQ(&fn, tensor->parentFunc());
+  ASSERT_EQ(nullptr, tensor->parentIns());
+  ASSERT_STREQ("`%c_0`: C@[<5, 1> {1.000, 2.000, 3.000, 4.000, 5.000}]",
+               tensor->string().c_str());
 }
 
 }  // namespace
