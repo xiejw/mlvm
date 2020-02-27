243cb601247f0c74e42483d48496c60b8485f266
Jianwei Xie <xiejw0217@gmail.com>
2020-02-26 21:25:10 -0800

    Started the work for prototype.

--- cmd/compile/main.cpp
+++ cmd/compile/main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <string>
+
+namespace mlvm {
+
+class Array {
+ public:
+  explicit Array(const char* value) : value_{std::string(value)} {};
+
+  std::string DebugString() const { return value_; }
+
+ private:
+  std::string value_;
+};
+
+class Tensor {
+  virtual std::string DebugString() const = 0;
+};
+
+class TConst : public Tensor {
+ public:
+  explicit TConst(Array arr) : arr_{std::move(arr)} {};
+
+  std::string DebugString() const override { return arr_.DebugString(); }
+
+ private:
+  Array arr_;
+};
+
+class TResult : public Tensor {};
+
+}  // namespace mlvm
 
 int main() {
-  std::cout << "hello\n";
+  mlvm::TConst c0{mlvm::Array{"c0"}};
+  std::cout << "const " << c0.DebugString() << "\n";
 }
