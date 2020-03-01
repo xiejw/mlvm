e941c0e6686f5f5455bf16bbe0ca8af9bcc96f8c
Jianwei Xie <xiejw0217@gmail.com>
2020-02-29 17:52:18 -0800

    Added newInstrunction.

--- cmd/compile/main.cpp
+++ cmd/compile/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -38,6 +39,9 @@ enum class OpType { Add };
 
 class Instruction {
  public:
+  Instruction(std::string name, OpType op, std::vector<Tensor*> inputs)
+      : name_{std::move(name)}, op_{op}, inputs_{std::move(inputs)} {};
+
   std::string DebugString() const {
     switch (op_) {
       case OpType::Add:
@@ -47,7 +51,9 @@ class Instruction {
   }
 
  private:
+  std::string name_;
   OpType op_;
+  std::vector<Tensor*> inputs_;
 };
 
 class Function {
@@ -59,6 +65,15 @@ class Function {
     return &consts_.back();
   };
 
+  template <typename... T>
+  Instruction* newInstruction(T&&... args) {
+    int count = instructions_.size();
+    auto ins = new Instruction("ins_" + std::to_string(count),
+                               std::forward<T>(args)...);
+    instructions_.push_back(std::unique_ptr<Instruction>{ins});
+    return instructions_.back().get();
+  }
+
   std::string DebugString() const {
     std::stringstream ss{};
     ss << "Function: `" << name_ << "`\n";
@@ -66,12 +81,20 @@ class Function {
     for (auto& c : consts_) {
       ss << "    - " << c.DebugString() << "\n";
     }
+
+    ss << "\n";
+    ss << "  Instructions:\n";
+    for (auto& ins : instructions_) {
+      ss << "    - " << ins->DebugString() << "\n";
+    }
+
     return ss.str();
   };
 
  private:
   std::string name_;
   std::vector<TConst> consts_;
+  std::vector<std::unique_ptr<Instruction>> instructions_;
 };
 
 }  // namespace mlvm
@@ -79,6 +102,9 @@ class Function {
 int main() {
   mlvm::Function fn{"main"};
   auto c0 = fn.newConst(mlvm::TConst{mlvm::Array{"c0"}});
+
+  fn.newInstruction(mlvm::OpType::Add, std::vector{c0, c0});
+
   std::cout << "const " << c0->DebugString() << "\n";
   std::cout << "func " << fn.DebugString() << "\n";
 }
