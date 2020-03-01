3d827620ea49cc3c28f49c0bf22f3931421eaea7
Jianwei Xie <xiejw0217@gmail.com>
2020-02-29 17:17:32 -0800

    Added function.

--- cmd/compile/main.cpp
+++ cmd/compile/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 namespace mlvm {
 
@@ -14,12 +16,15 @@ class Array {
 };
 
 class Tensor {
+ public:
+  virtual ~Tensor(){};
   virtual std::string DebugString() const = 0;
 };
 
 class TConst : public Tensor {
  public:
   explicit TConst(Array arr) : arr_{std::move(arr)} {};
+  ~TConst() override{};
 
   std::string DebugString() const override { return arr_.DebugString(); }
 
@@ -45,9 +50,35 @@ class Instruction {
   OpType op_;
 };
 
+class Function {
+ public:
+  Function(std::string name) : name_{name} {};
+
+  Tensor* newConst(TConst&& c) {
+    consts_.push_back(std::move(c));
+    return &consts_.back();
+  };
+
+  std::string DebugString() const {
+    std::stringstream ss{};
+    ss << "Function: `" << name_ << "`\n";
+    ss << "  Consts:\n";
+    for (auto& c : consts_) {
+      ss << "    - " << c.DebugString() << "\n";
+    }
+    return ss.str();
+  };
+
+ private:
+  std::string name_;
+  std::vector<TConst> consts_;
+};
+
 }  // namespace mlvm
 
 int main() {
-  mlvm::TConst c0{mlvm::Array{"c0"}};
-  std::cout << "const " << c0.DebugString() << "\n";
+  mlvm::Function fn{"main"};
+  auto c0 = fn.newConst(mlvm::TConst{mlvm::Array{"c0"}});
+  std::cout << "const " << c0->DebugString() << "\n";
+  std::cout << "func " << fn.DebugString() << "\n";
 }
