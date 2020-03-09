6638013482400f037c1625fe8e970ae2c23b47c4
Jianwei Xie <xiejw0217+xiejw2g@gmail.com>
2020-03-09 13:25:57 -0700

    Turns off static build by default.

--- cmake/MLVMHelpers.cmake
+++ cmake/MLVMHelpers.cmake
@@ -1,8 +1,10 @@
 # {{{1 Common Function
-function(add_mlvm_executable NAME CPP_FILE)
+function(add_mlvm_executable NAME CPP_FILE STATIC)
   add_executable(${NAME} ${CPP_FILE})
-  if(UNIX AND NOT APPLE)
-    message("Build static binary for ${NAME}")
-    target_link_libraries(${NAME} -static)  # Static binary.
+  if(${STATIC})
+    message("Enable build static binary for ${NAME}")
+    target_link_libraries(${NAME} -static)
+  else()
+    message("Disable build static binary for ${NAME}")
   endif()
 endfunction()
