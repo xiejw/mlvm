b6233740006a5075b71cc10c6627b9645b63a563
Jianwei Xie <xiejw0217@gmail.com>
2020-03-26 10:37:49 -0700

    Tested gtest.

--- cmd/test/main.cc
+++ cmd/test/main.cc
@@ -1,5 +1,13 @@
 #include "gtest/gtest.h"
 
+namespace {
+
+class FoundationStatusOrTest : public ::testing::Test {};
+
+TEST_F(FoundationStatusOrTest, CheckStatus) { ASSERT_TRUE(2 > 1); }
+
+}  // namespace
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
