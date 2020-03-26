956a797eab444306913495f080c536ba8316f274
Jianwei Xie <xiejw0217@gmail.com>
2020-03-25 18:14:20 -0700

    Improved Instruction output.

--- mlvm/IR/Instruction.cc
+++ mlvm/IR/Instruction.cc
@@ -8,6 +8,7 @@ namespace mlvm {
 
 std::string Instruction::debugString() const {
   std::stringstream ss{};
+  // Push type.
   switch (op_) {
     case OpType::Add:
       ss << "Add";
@@ -15,9 +16,21 @@ std::string Instruction::debugString() const {
     default:
       ss << "Unknown Op";
   }
+  // Push name.
   ss << " `" << name_ << "`";
-  ss << " (" << inputs_.size() << " inputs and " << outputs_.size()
-     << " outputs)";
+  // Push operands.
+  ss << " (";
+  for (decltype(inputs_.size()) i = 0; i < inputs_.size(); ++i) {
+    if (i != 0) ss << ", ";
+    ss << inputs_[i]->debugString();
+  }
+  ss << ") -> (";
+  // Push outputs
+  for (decltype(outputs_.size()) i = 0; i < outputs_.size(); ++i) {
+    if (i != 0) ss << ", ";
+    ss << outputs_[i]->debugString();
+  }
+  ss << ")";
   return ss.str();
 }
 
--- mlvm/IR/Tensor.cc
+++ mlvm/IR/Tensor.cc
@@ -1,7 +1,11 @@
 #include "mlvm/IR/Tensor.h"
 
+#include <absl/strings/str_cat.h>
+
 namespace mlvm {
 
-std::string Array::debugString() const { return value_; }
+std::string Array::debugString() const {
+  return absl::StrCat("`", value_, "`");
+}
 
 }  // namespace mlvm
