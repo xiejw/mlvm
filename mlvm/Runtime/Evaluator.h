f491b0b019e36a621e83b7720875d3dafda1f7c6
Jianwei Xie <xiejw0217@gmail.com>
2020-03-26 19:07:39 -0700

    Added evaluator.

---
+++ mlvm/Runtime/Evaluator.h
@@ -0,0 +1,15 @@
+#ifndef MLVM_RUNTIME_EVALUATOR_H_
+#define MLVM_RUNTIME_EVALUATOR_H_
+
+#include <string>
+
+#include "mlvm/IR/Function.h"
+
+namespace mlvm {
+
+class Evaluator {
+};
+
+}  // namespace mlvm
+
+#endif
