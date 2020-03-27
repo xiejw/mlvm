eb142306dfe03dc6fdf59fa8b3cc1a35a2008d58
Jianwei Xie <xiejw0217@gmail.com>
2020-03-27 12:42:09 -0700

    Refactoring the main.cc.

--- cmd/demo/main.cc
+++ cmd/demo/main.cc
@@ -3,16 +3,12 @@
 #include "mlvm/IR/Function.h"
 #include "mlvm/Runtime/Evaluator.h"
 
-namespace mlvm {}  // namespace mlvm
-
 int main() {
   mlvm::IR::Function fn{"main"};
   auto c0 = fn.newConst(mlvm::IR::ConstTensor{mlvm::IR::Array{"c0"}});
-
   auto ins = fn.newInstruction(mlvm::IR::OpType::Add, std::vector{c0, c0});
   auto o0 = ins->outputAt(0);
   ins = fn.newInstruction(mlvm::IR::OpType::Add, std::vector{o0, o0});
-
   MLVM_FATAL_IF_ERROR(fn.setOutput(ins->outputAt(0)));
 
   std::cout << "Func:\n" << fn.debugString() << "\n";
