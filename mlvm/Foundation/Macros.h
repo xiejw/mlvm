a253b47a70b27e282197b52b42cd3b738bd9fb40
Jianwei Xie <xiejw0217@gmail.com>
2020-03-26 18:10:32 -0700

    Added check and precondition.

--- mlvm/Foundation/Macros.h
+++ mlvm/Foundation/Macros.h
@@ -1,9 +1,18 @@
 #ifndef MLVM_FOUNDATION_MACROS_H_
 #define MLVM_FOUNDATION_MACROS_H_
 
+#include <cassert>
 #include <string>
 
 #include "mlvm/Foundation/Utilities.h"
+
+#define MLVM_CHECK(x) assert(x)
+#define MLVM_PRECONDITION(x)                             \
+  {                                                      \
+    if (!(x)) {                                          \
+      FatalError("Precondition failed at %s", __FILE__); \
+    }                                                    \
+  }
 
 // Returns the function with error status if x.ok() == false.
 #define MLVM_RETURN_IF_ERROR(x)      \
