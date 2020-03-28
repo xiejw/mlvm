7a8a24347bf6d2f535613429e3c2d966a461da57
Jianwei Xie <xiejw0217@gmail.com>
2020-03-28 11:10:49 -0700

    Added function in main.

--- cmd/demo/main.cc
+++ cmd/demo/main.cc
@@ -1,18 +1,33 @@
 #include <iostream>
+#include <memory>
 
 #include "mlvm/IR/Function.h"
 #include "mlvm/Runtime/Evaluator.h"
+#include "mlvm/Foundation/StatusOr.h"
+
+mlvm::StatusOr<std::unique_ptr<mlvm::IR::Function>> buildFunction() {
+  auto fn = std::make_unique<mlvm::IR::Function>("main");
+  auto c0 = fn->newConst(mlvm::IR::ConstTensor{mlvm::IR::Array{"c0"}});
+  auto ins = fn->newInstruction(mlvm::IR::OpType::Add, std::vector{c0, c0});
+  auto o0 = ins->outputAt(0);
+  ins = fn->newInstruction(mlvm::IR::OpType::Add, std::vector{o0, o0});
+  MLVM_FATAL_IF_ERROR(fn->setOutput(ins->outputAt(0)));
+  return fn;
+}
 
 int main() {
-  mlvm::IR::Function fn{"main"};
-  auto c0 = fn.newConst(mlvm::IR::ConstTensor{mlvm::IR::Array{"c0"}});
-  auto ins = fn.newInstruction(mlvm::IR::OpType::Add, std::vector{c0, c0});
-  auto o0 = ins->outputAt(0);
-  ins = fn.newInstruction(mlvm::IR::OpType::Add, std::vector{o0, o0});
-  MLVM_FATAL_IF_ERROR(fn.setOutput(ins->outputAt(0)));
 
-  std::cout << "Func:\n" << fn.debugString() << "\n";
+
+  auto status_or = buildFunction();
+  if (!status_or.ok()) {
+    std::cout << "Error";
+    return -1;
+  }
+
+  auto fn = status_or.comsumeValue();
+
+  std::cout << "Func:\n" << fn->debugString() << "\n";
 
   mlvm::RT::Evaluator eval{};
-  MLVM_FATAL_IF_ERROR(eval.run(fn));
+  MLVM_FATAL_IF_ERROR(eval.run(*fn));
 }
