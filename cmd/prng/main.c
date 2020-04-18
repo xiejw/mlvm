1430afd6b9bc4cbd3884e9464addd2a0ec2f0155
Jianwei Xie <xiejw0217@gmail.com>
2020-04-17 22:37:23 -0700

    Added some helper methods.

--- cmd/prng/main.c
+++ cmd/prng/main.c
@@ -13,8 +13,27 @@ static const double double_ulp_ = 1.0 / (1L << 53);
 #endif
 
 typedef struct {
+  /* Internal fields. */
   uint64_t seed_;
-} splittable64_t;
+  uint64_t gamma_;
+  uint64_t next_split_;
+} sprng64_t;
+
+static uint64_t sprng64_update(sprng64_t* prng, uint64_t s, uint64_t g) {
+  /* Add g to s modulo George. */
+  uint64_t p = s + g;
+  return (p >= s) ? p : (p >= 0x800000000000000DL) ? p - 13L : (p - 13L) + g;
+}
+
+static uint64_t sprng64_mix56(sprng64_t* prng, uint64_t z) {
+  z = ((z ^ (z >> 33)) * 0xff51afd7ed558ccdL) & 0x00FFFFFFFFFFFFFFL;
+  z = ((z ^ (z >> 33)) * 0xc4ceb9fe1a85ec53L) & 0x00FFFFFFFFFFFFFFL;
+  return z ^ (z >> 33);
+}
+
+static uint64_t sprng64_next_raw64(sprng64_t* prng) {
+  return (prng->seed_ = sprng64_update(prng, prng->seed_, prng->gamma_));
+}
 
 int main() {
   debug_printf("prime_ %llx\n", gamma_prime_);
