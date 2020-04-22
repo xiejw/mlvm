93dab980165605199cbc9f0ba8958ad13d1800ff
Jianwei Xie <xiejw0217@gmail.com>
2020-04-22 14:40:54 -0700

    Added normal test.

--- mlvm/testing/test.h
+++ mlvm/testing/test.h
@@ -1,7 +1,14 @@
 #ifndef MLVM_TESTING_TEST_H_
 #define MLVM_TESTING_TEST_H_
 
-#include <stdio.h>
+#include <math.h>  /* fabs */
+#include <stdio.h> /* printf */
+
+#ifdef DEBUG_TEST_ASSERT
+#define DEBUG_TEST_PRINTF(x, ...) printf(x, __VA_ARGS__)
+#else
+#define DEBUG_TEST_PRINTF(x, ...)
+#endif
 
 /* This unit testing framework is inspired by MinUnit. */
 
@@ -32,6 +39,18 @@ extern int tests_run;
 #define ASSERT_TRUE(msg, test) \
   do {                         \
     if (!(test)) return msg;   \
+  } while (0)
+
+#define ASSERT_ARRAY_CLOSE(msg, expected, got, size, tol)                     \
+  do {                                                                        \
+    int i;                                                                    \
+    for (i = 0; i < size; i++) {                                              \
+      if (fabs(expected[i] - got[i]) >= tol) {                                \
+        DEBUG_TEST_PRINTF("\n-> at element %d\n-> got %f\n-> expect %f\n", i, \
+                          expected[i], got[i]);                               \
+        return msg;                                                           \
+      }                                                                       \
+    }                                                                         \
   } while (0)
 
 #endif
