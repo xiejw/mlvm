b919455b546b4ae2ee84da26abd51e8529bbf8db
Jianwei Xie <xiejw0217@gmail.com>
2020-05-04 11:49:29 -0700

    Added set_prng.

--- mlvm/ir/context.c
+++ mlvm/ir/context.c
@@ -6,12 +6,19 @@
 
 ir_context_t* ir_context_create() {
   ir_context_t* ctx       = malloc(sizeof(ir_context_t));
+  ctx->prng               = NULL;
   ctx->error_message_cap_ = MLVM_IR_DEFAULT_ERROR_MSG_CAP;
   ctx->error_message = malloc(MLVM_IR_DEFAULT_ERROR_MSG_CAP * sizeof(char));
   return ctx;
 }
 
 void ir_context_free(ir_context_t* ctx) {
+  if (ctx->prng != NULL) sprng_free(ctx->prng);
   free(ctx->error_message);
   free(ctx);
+}
+
+void ir_context_set_prng(ir_context_t* ctx, sprng_t* prng) {
+  if (ctx->prng != NULL) sprng_free(ctx->prng);
+  ctx->prng = prng;
 }
--- mlvm/ir/context.h
+++ mlvm/ir/context.h
@@ -1,8 +1,11 @@
 #ifndef MLVM_IR_CONTEXT_H_
 #define MLVM_IR_CONTEXT_H_
 
+#include "mlvm/sprng/sprng.h"
+
 typedef struct {
-  char* error_message;
+  char*    error_message;
+  sprng_t* prng; /* Optional. */
 
   /* Internal fields. */
   unsigned int error_message_cap_;
@@ -10,5 +13,8 @@ typedef struct {
 
 extern ir_context_t* ir_context_create();
 extern void          ir_context_free();
+
+/* Move the prng into the context. Currrent `prng` is freed. */
+extern void ir_context_set_prng(ir_context_t* ctx, sprng_t* prng);
 
 #endif
