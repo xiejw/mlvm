efa0e8216e718dd4513464446737f0b16a6e4a23
Jianwei Xie <xiejw0217@gmail.com>
2020-05-15 14:50:32 -0700

    Added parallel version.

--- benchmarks/matmul.go
+++ benchmarks/matmul.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
+	"runtime"
 )
 
 const (
@@ -24,7 +26,7 @@ func gemm(A, B, C []float32) {
 }
 
 // Elapsed 2m8.324053419s
-func gemmLoopOrder(A, B, C []float32) {
+func gemmLoopOrderNaive(A, B, C []float32) {
 	for i := 0; i < M; i++ {
 		for k := 0; k < K; k++ {
 			for j := 0; j < N; j++ {
@@ -32,6 +34,66 @@ func gemmLoopOrder(A, B, C []float32) {
 			}
 		}
 	}
+}
+
+// Elapsed 1m31.383356293s
+func gemmLoopOrder(A, B, C []float32) {
+	for i := 0; i < M; i++ {
+		for k := 0; k < K; k++ {
+			a := A[i*K+k]
+			indexC := i * N
+			indexB := k * N
+
+			for j := 0; j < N; j++ {
+				C[indexC] += a * B[indexB]
+				indexC++
+				indexB++
+			}
+		}
+	}
+}
+
+// Elapsed 17.097657839s
+func gemmLoopOrderWithParallelism(A, B, C []float32, numThreads int) {
+	wg := new(sync.WaitGroup)
+
+	var delta int
+	delta =  M/numThreads
+	if M % numThreads != 0 {
+		delta+=1
+	}
+
+	for p := 0; p < numThreads; p++ {
+
+		wg.Add(1)
+
+		go func(start int) {
+
+			startIndex := start * delta
+			endIndex := startIndex + delta
+			if endIndex >= M {
+				endIndex = M - 1
+			}
+
+			for i := startIndex; i < endIndex; i++ {
+				for k := 0; k < K; k++ {
+					a := A[i*K+k]
+					indexC := i * N
+					indexB := k * N
+
+					for j := 0; j < N; j++ {
+						C[indexC] += a * B[indexB]
+						indexC++
+						indexB++
+					}
+				}
+			}
+			wg.Done()
+
+		}(p)
+
+	}
+	wg.Wait()
 }
 
 func main() {
@@ -50,14 +112,20 @@ func main() {
 		B[i] = rand.Float32()
 	}
 
+	numCPU := runtime.NumCPU()
+
+	fmt.Printf("Num CPU: %v\n", numCPU)
+
 	start := time.Now()
 
 	// gemm(A, B, C)
-	gemmLoopOrder(A, B, C)
+	// gemmLoopOrderNaive(A, B, C)
+	// gemmLoopOrder(A, B, C)
+	 gemmLoopOrderWithParallelism(A, B, C, numCPU)
 
 	end := time.Now()
-
 	fmt.Printf("Elapsed %v\n", end.Sub(start))
+
 
 	for i := 0; i < sizeC; i++ {
 		if i == 100 {
