128e75169febc8cc6bc98c29b1526bae1a368d0d
Jianwei Xie <xiejw0217@gmail.com>
2020-05-18 11:28:57 -0700

    Added memory set and get.

--- go/vm/runtime/memory.go
+++ go/vm/runtime/memory.go
@@ -1,21 +1,45 @@
 package runtime
 
+import (
+	"errors"
+	"fmt"
+)
+
 const defaultMemoryInitSize = 256
 
+var (
+	errRetrieveFromInvalidSlot = "Retrieves an item from in valid slot at: %d (current memory size: %d)."
+)
+
+// An abstraction of memory to store and load item from slots. If the size is not enough, internally
+// the memory size will be enlarged automatically.
+//
+// Internally, we could use a slice or use list of pages.
 type Memory struct {
-	items []interface{}
+	slots []interface{}
 	size  int
 }
 
 func NewMemory() *Memory {
 	memory := &Memory{
-		items: make([]interface{}, defaultMemoryInitSize),
+		slots: make([]interface{}, defaultMemoryInitSize),
 		size:  defaultMemoryInitSize,
 	}
 
 	return memory
 }
 
-func (m *Memory) Get(index int) interface{} {
-	return m.items[index]
+func (m *Memory) Get(index int) (interface{}, error) {
+	if index >= m.size {
+		return nil, errors.New(fmt.Sprintf(errRetrieveFromInvalidSlot, index, m.size))
+	}
+	return m.slots[index], nil
+}
+
+func (m *Memory) Set(index int, item interface{}) error {
+	if index >= m.size {
+		panic("Index is too large. Enlarging is planning.")
+	}
+	m.slots[index] = item
+	return nil
 }
