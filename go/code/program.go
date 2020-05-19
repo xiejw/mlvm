ed9300d4a14a4236704e1572fb36c4c508c5cdd9
Jianwei Xie <xiejw0217@gmail.com>
2020-05-19 08:38:14 -0700

    Added simple program test.

--- go/code/program.go
+++ go/code/program.go
@@ -1,6 +1,15 @@
 package code
 
+const defaultProgramInitSize = 128
+
 type Program struct {
 	Instructions Instructions
 	Constants    []interface{} // change to object.
+}
+
+func NewProgram() *Program {
+	return &Program{
+		Instructions: make([]byte, 0, defaultProgramInitSize),
+		Constants:    make([]interface{}, 0),
+	}
 }
