3fd271d67cb9e89dd24495ac0a2201c11628a346
Jianwei Xie <xiejw0217@gmail.com>
2020-05-23 16:31:19 -0700

    Added truncnorm and test.

---
+++ go/object/prng64/dist_truncnorm_test.go
@@ -0,0 +1,24 @@
+package prng64
+
+import (
+	"testing"
+)
+
+func TestTrucatedNorm(t *testing.T) {
+	size := 5
+	value := make([]float32, size)
+
+	// First assert norm generates the number outside the bounds, for this seed.
+	prng := NewPrng64(123456789)
+	prng.Norm(value)
+	expected := []float32{1.1747507, 2.3442311, -1.0833434, -0.26150864, -0.08424469}
+	assertAllClose(t, expected, value, 1e-6)
+
+	// Then assert that number is skipped.
+	size = 4
+	value = make([]float32, size)
+	prng = NewPrng64(123456789)
+	prng.TruncatedNorm(value)
+	expected = []float32{1.1747507, -1.0833434, -0.26150864, -0.08424469}
+	assertAllClose(t, expected, value, 1e-6)
+}
