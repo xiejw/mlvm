5f32b96d062a64a160e68aaf9a5e74b68dc5b92f
Jianwei Xie <xiejw0217@gmail.com>
2020-06-01 09:22:20 -0700

    Started adding types.

--- go/syntax/ast/node.go
+++ go/syntax/ast/node.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"github.com/xiejw/mlvm/go/object"
+)
+
 type Node interface {
 	TokenLiteral() string
 	String() string
@@ -7,4 +11,55 @@ type Node interface {
 
 type Statement interface {
 	Node
+}
+
+// Represents the Kind (enum) of Type of Expression.
+type TypeKind uint
+
+const (
+	TKVoid TypeKind = iota
+	TKInt
+	TKString
+	TKArray
+	TKNameDimension
+	TKTensor
+	TKFunc
+)
+
+// Represents a parameter in a func signature.
+type Param struct {
+	Name string
+	Type *Type
+}
+
+// Represents Type of Expression.
+//
+// For Array, the base type is SubType.
+type Type struct {
+	Kind    TypeKind
+	SubType *Type
+	Params  []Param
+}
+
+type Decl struct {
+	Name  string
+	Type  *Type
+	Value *Expression
+	Code  Statement
+	Next  *Decl
+}
+
+type ExpressionType uint
+
+const (
+	ExTValue ExpressionType = iota
+	ExTAdd
+	ExTMul
+)
+
+type Expression struct {
+	Type  ExpressionType
+	Left  *Expression
+	Right *Expression
+	Value object.Object
 }
