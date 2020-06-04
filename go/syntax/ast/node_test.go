ba2f4ab46792774d6eec9f366df9d83201bb36fe
Jianwei Xie <xiejw0217@gmail.com>
2020-06-03 19:05:48 -0700

    Added assert.

--- go/syntax/ast/node_test.go
+++ go/syntax/ast/node_test.go
@@ -7,8 +7,17 @@ import (
 	"github.com/xiejw/mlvm/go/object"
 )
 
+func assertStringRepr(t *testing.T, expectedStartedWithNewLine, got string) {
+
+	expected := expectedStartedWithNewLine[1:]
+	if got[:] != expected[:] {
+		t.Errorf("String representation mismatches.\n-> expected:\n%v\n-> got:\n%v\n",
+			expected, got)
+	}
+}
+
 func TestDeclNamedDim(t *testing.T) {
-	// @batch = 32;
+	// let @batch = 32;
 	decl := &Decl{
 		Name: "@batch",
 		Type: &Type{Kind: TpKdNamedDim},
@@ -20,9 +29,10 @@ func TestDeclNamedDim(t *testing.T) {
 
 	var buf bytes.Buffer
 	decl.WriteDebugString(&buf, "")
-	outs := buf.String()
+	got := buf.String()
 
-	expected := `Decl{
+	expected := `
+Decl{
   Name: "@batch"
   Type: Type(NamedDim)
   Value: Expr{
@@ -30,7 +40,6 @@ func TestDeclNamedDim(t *testing.T) {
   }
 }
 `
-	if outs != expected {
-		t.Errorf("Mismatches. expected:\n%v\ngot:\n%v\n", expected, outs)
-	}
+
+	assertStringRepr(t, expected, got)
 }
