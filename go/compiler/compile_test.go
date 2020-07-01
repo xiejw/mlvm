2022529fc6d69b49ecd8fa47244c3e1540da2114
Jianwei Xie <xiejw0217@gmail.com>
2020-06-30 18:00:46 -0700

    added test for compile.

---
+++ go/compiler/compile_test.go
@@ -0,0 +1,59 @@
+package compiler
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/xiejw/mlvm/go/code"
+	"github.com/xiejw/mlvm/go/syntax/ast"
+)
+
+func assertNoErr(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func assertInstructions(t *testing.T, expected, got string) {
+	t.Helper()
+	expected = strings.Trim(expected, "\n")
+	got = strings.Trim(got, "\n")
+	if expected != got {
+		t.Fatalf(
+			"instructions mismatch. "+
+				"expected:\n======\n%v\n======\n"+
+				"got:\n=====\n%v\n=====",
+			expected, got)
+	}
+}
+
+func makeOpHelper(t *testing.T, op code.Opcode, args ...int) []byte {
+	t.Helper()
+	ins, err := code.MakeOp(op, args...)
+	assertNoErr(t, err)
+
+	return ins
+}
+
+func TestExprSingleLiteral(t *testing.T) {
+	statements := []ast.Statement{
+		&ast.ExprStatement{
+			Value: &ast.IntegerLiteral{123},
+		}}
+
+	p := &ast.Program{
+		Statements: statements,
+	}
+
+	o, err := Compile(p)
+	assertNoErr(t, err)
+
+	got := o.Instructions.String()
+
+	var ins code.Instructions
+	ins = append(ins, makeOpHelper(t, code.OpConstant, 0)...)
+	expected := ins.String()
+
+	assertInstructions(t, expected, got)
+}
