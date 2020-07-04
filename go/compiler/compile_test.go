4d45edc9c493019bafe0c4fd4fa6ad3b979a0039
Jianwei Xie <xiejw0217@gmail.com>
2020-07-04 15:21:59 -0700

    commented out tests.

--- go/compiler/compile_test.go
+++ go/compiler/compile_test.go
@@ -1,78 +1,78 @@
 package compiler
 
-import (
-	"strings"
-	"testing"
-
-	"github.com/xiejw/mlvm/go/code"
-	"github.com/xiejw/mlvm/go/syntax/ast"
-)
-
-func assertNoErr(t *testing.T, err error) {
-	t.Helper()
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-}
-
-func assertInstructions(t *testing.T, expected, got string) {
-	t.Helper()
-	expected = strings.Trim(expected, "\n")
-	got = strings.Trim(got, "\n")
-	if expected != got {
-		t.Fatalf(
-			"instructions mismatch. "+
-				"expected:\n======\n%v\n======\n"+
-				"got:\n=====\n%v\n=====",
-			expected, got)
-	}
-}
-
-func makeOpHelper(t *testing.T, op code.Opcode, args ...int) []byte {
-	t.Helper()
-	ins, err := code.MakeOp(op, args...)
-	assertNoErr(t, err)
-
-	return ins
-}
-
-func TestExprSingleLiteral(t *testing.T) {
-	c, err := Compile(&ast.Program{
-		Statements: []ast.Statement{
-			&ast.ExprStatement{
-				Value: &ast.IntegerLiteral{123},
-			}},
-	})
-	assertNoErr(t, err)
-	got := c.Instructions.String()
-
-	var ins code.Instructions
-	ins = append(ins, makeOpHelper(t, code.OpConstant, 0)...)
-	expected := ins.String()
-
-	assertInstructions(t, expected, got)
-}
-
-func TestExprTensorStoreLoad(t *testing.T) {
-	c, err := Compile(&ast.Program{
-		Statements: []ast.Statement{
-			&ast.ExprStatement{
-				Value: &ast.FunctionCall{
-					Name: &ast.Identifier{"store_load"},
-					Args: []ast.Expression{
-						&ast.StringLiteral{"a"},
-					},
-				},
-			},
-		},
-	})
-	assertNoErr(t, err)
-	got := c.Instructions.String()
-
-	var ins code.Instructions
-	ins = append(ins, makeOpHelper(t, code.OpConstant, 0)...)
-	ins = append(ins, makeOpHelper(t, code.OpLoadT)...)
-	expected := ins.String()
-
-	assertInstructions(t, expected, got)
-}
+// import (
+// 	"strings"
+// 	"testing"
+//
+// 	"github.com/xiejw/mlvm/go/code"
+// 	"github.com/xiejw/mlvm/go/syntax/ast"
+// )
+//
+// func assertNoErr(t *testing.T, err error) {
+// 	t.Helper()
+// 	if err != nil {
+// 		t.Fatalf("unexpected error: %v", err)
+// 	}
+// }
+//
+// func assertInstructions(t *testing.T, expected, got string) {
+// 	t.Helper()
+// 	expected = strings.Trim(expected, "\n")
+// 	got = strings.Trim(got, "\n")
+// 	if expected != got {
+// 		t.Fatalf(
+// 			"instructions mismatch. "+
+// 				"expected:\n======\n%v\n======\n"+
+// 				"got:\n=====\n%v\n=====",
+// 			expected, got)
+// 	}
+// }
+//
+// func makeOpHelper(t *testing.T, op code.Opcode, args ...int) []byte {
+// 	t.Helper()
+// 	ins, err := code.MakeOp(op, args...)
+// 	assertNoErr(t, err)
+//
+// 	return ins
+// }
+//
+// func TestExprSingleLiteral(t *testing.T) {
+// 	c, err := Compile(&ast.Program{
+// 		Statements: []ast.Statement{
+// 			&ast.ExprStatement{
+// 				Value: &ast.IntegerLiteral{123},
+// 			}},
+// 	})
+// 	assertNoErr(t, err)
+// 	got := c.Instructions.String()
+//
+// 	var ins code.Instructions
+// 	ins = append(ins, makeOpHelper(t, code.OpConstant, 0)...)
+// 	expected := ins.String()
+//
+// 	assertInstructions(t, expected, got)
+// }
+//
+// func TestExprTensorStoreLoad(t *testing.T) {
+// 	c, err := Compile(&ast.Program{
+// 		Statements: []ast.Statement{
+// 			&ast.ExprStatement{
+// 				Value: &ast.FunctionCall{
+// 					Name: &ast.Identifier{"store_load"},
+// 					Args: []ast.Expression{
+// 						&ast.StringLiteral{"a"},
+// 					},
+// 				},
+// 			},
+// 		},
+// 	})
+// 	assertNoErr(t, err)
+// 	got := c.Instructions.String()
+//
+// 	var ins code.Instructions
+// 	ins = append(ins, makeOpHelper(t, code.OpConstant, 0)...)
+// 	ins = append(ins, makeOpHelper(t, code.OpLoadT)...)
+// 	expected := ins.String()
+//
+// 	assertInstructions(t, expected, got)
+// }
--- go/syntax/ast/node_test.go
+++ go/syntax/ast/node_test.go
@@ -1,17 +1,17 @@
 package ast
 
-import "testing"
-
-func TestSingleExprStatement(t *testing.T) {
-	p := Program{
-		Statements: []Statement{
-			&ExprStatement{
-				Value: &IntegerLiteral{123},
-			},
-		},
-	}
-
-	if len(p.Statements) != 1 {
-		t.Errorf("expect empty Program")
-	}
-}
+// import "testing"
+//
+// func TestSingleExprStatement(t *testing.T) {
+// 	p := Program{
+// 		Statements: []Statement{
+// 			&ExprStatement{
+// 				Value: &IntegerLiteral{123},
+// 			},
+// 		},
+// 	}
+//
+// 	if len(p.Statements) != 1 {
+// 		t.Errorf("expect empty Program")
+// 	}
+// }
