2b11f705e94d08cee223d7163ecee6392491f87f
Jianwei Xie <xiejw0217@gmail.com>
2020-07-04 17:04:47 -0700

    add add.

--- go/compiler/parser/parser.go
+++ go/compiler/parser/parser.go
@@ -28,7 +28,7 @@ func (p *Parser) Loop() {
 	for {
 		tok := p.l.NextToken()
 		if p.option.Trace {
-			log.Printf("parser got token from lexer: %+v\n", tok)
+			log.Printf("trace parser: got next token: %+v\n", tok)
 		}
 		if tok.Type == token.EOF {
 			break
