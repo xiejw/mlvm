f570f8401bdef78f7256d82d3767fe037e025955
Jianwei Xie <xiejw0217@gmail.com>
2020-07-22 20:37:53 -0700

    added token.

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 default:
+	cargo run
+
+compile:
 	cargo build
 
 fmt:
 	cargo fmt
+
+clean:
+	cargo clean
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod mlvm {
+    pub mod syntax {
+        pub mod lexer {
+
+            #[derive(Debug)]
+            pub enum Token {
+                Lparen,
+                Rparen,
+                Lsbracket,
+                Rsbracket,
+                Backslash,
+                Identifier,
+                Integer,
+                Float,
+                String,
+                Illegal,
+                Eof,
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {:?}", mlvm::syntax::lexer::Token::Lparen);
 }
