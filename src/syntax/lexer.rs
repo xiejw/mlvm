26619266e87c7d4f0bf38d24406f6f3b7d616f35
Jianwei Xie <xiejw0217@gmail.com>
2020-07-23 17:50:51 -0700

    added bytes.

--- src/syntax/lexer.rs
+++ src/syntax/lexer.rs
@@ -1,7 +1,7 @@
 use super::token;
 
-pub struct Lexer {
-    input: Vec<u8>,
+pub struct Lexer<'a> {
+    input: &'a [u8],
     size: usize,
     pos: usize,
     read_pos: usize,
@@ -9,10 +9,10 @@ pub struct Lexer {
     loc: token::Loc,
 }
 
-impl Lexer {
-    pub fn new(input: Vec<u8>) -> Lexer {
+impl Lexer<'_> {
+    pub fn new<'a>(input: &'a [u8]) -> Lexer<'a> {
         let size = input.len();
-        Lexer {
+        let mut l = Lexer {
             input: input,
             size: size,
             pos: 0,
@@ -23,11 +23,24 @@ impl Lexer {
                 col: 0,
                 pos: 0,
             },
+        };
+
+        l.read_char();
+        l
+    }
+
+    pub fn bytes<'a>(self: &'a Self, start: usize, end: usize) -> Option<&'a [u8]> {
+        if end >= self.size {
+            return None
         }
+        if start > end {
+            return Nne
+        }
+        Some(&self.input[start..end])
     }
 }
 
-impl Lexer {
+impl Lexer<'_> {
     fn read_char(self: &mut Self) {
         if self.read_pos >= self.size {
             self.ch = 0;
@@ -45,9 +58,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_lexer() {
-        let mut l = Lexer::new(b"ab".to_vec());
-        l.read_char();
+    fn test_lexer_read_char() {
+        let mut l = Lexer::new(b"ab");
         assert_eq!(b'a', l.ch);
+        assert_eq!(1, l.read_pos);
+        assert_eq!(0, l.pos);
+        l.read_char();
+
+        assert_eq!(b'b', l.ch);
+        assert_eq!(2, l.read_pos);
+        assert_eq!(1, l.pos);
+    }
+
+    #[test]
+    fn test_lexer_bytes() {
+        let l = Lexer::new(b"ab");
+        assert_eq!(b"a", l.bytes(0, 1));
+        assert_eq!(b"ab", l.bytes(0, 2));
+        assert_eq!(b"ab", l.bytes(0, 3));
     }
 }
