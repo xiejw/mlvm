433ef5d4c40c8578254e34a282e6ce06bfe406f9
Jianwei Xie <xiejw0217@gmail.com>
2020-07-25 15:48:58 -0700

    added floatlt.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ use mlvm::syntax::ast::Type;
 fn main() {
     let mut p = Program::new(vec![Expr::IntLt(Type::Float, 123)]);
     print!("before:\n{}", p);
+
     if let Err(err) = p.infer_types() {
         print!("\nunexpected error:\n{}\n", err);
         return;
     };
+
     print!("after:\n{}", p);
 }
--- src/syntax/ast/mod.rs
+++ src/syntax/ast/mod.rs
@@ -39,6 +39,19 @@ impl Program {
                         ))
                         .take()),
                 },
+                Expr::FloatLt(ref mut tp, _) => match tp {
+                    Type::Float => Ok(()),
+                    Type::Unknown => {
+                        *tp = Type::Float;
+                        Ok(())
+                    }
+                    _ => Err(Error::new()
+                        .emit_diagnosis_note(format!(
+                            "Float Literal should have type Float. Got: {}",
+                            tp
+                        ))
+                        .take()),
+                },
                 _ => Err(Error::new()
                     .emit_diagnosis_note_str("un supported expr type yet")
                     .take()),
@@ -82,13 +95,13 @@ mod tests {
     }
 
     #[test]
-    fn test_infer_int() {
+    fn test_infer_intlt() {
         let mut p = Program::new(vec![Expr::new_intlt(123)]);
         p.infer_types().unwrap();
     }
 
     #[test]
-    fn test_infer_int_with_unknown_type() {
+    fn test_infer_intlt_with_unknown_type() {
         let mut p = Program::new(vec![Expr::IntLt(Type::Unknown, 123)]);
         p.infer_types().unwrap();
         assert_eq!(&Expr::IntLt(Type::Int, 123), &p.exprs[0]);
@@ -96,8 +109,28 @@ mod tests {
 
     #[test]
     #[should_panic = "should have type Int. Got: Float"]
-    fn test_infer_int_with_wrong_type() {
+    fn test_infer_intlt_with_wrong_type() {
         let mut p = Program::new(vec![Expr::IntLt(Type::Float, 123)]);
+        p.infer_types().unwrap();
+    }
+
+    #[test]
+    fn test_infer_floatlt() {
+        let mut p = Program::new(vec![Expr::new_floatlt(123.3)]);
+        p.infer_types().unwrap();
+    }
+
+    #[test]
+    fn test_infer_floatlt_with_unknown_type() {
+        let mut p = Program::new(vec![Expr::FloatLt(Type::Unknown, 123.3)]);
+        p.infer_types().unwrap();
+        assert_eq!(&Expr::FloatLt(Type::Float, 123.3), &p.exprs[0]);
+    }
+
+    #[test]
+    #[should_panic = "should have type Float. Got:"]
+    fn test_infer_floatlt_with_wrong_type() {
+        let mut p = Program::new(vec![Expr::FloatLt(Type::Int, 123.)]);
         p.infer_types().unwrap();
     }
 }
