ca9e47f4a578f42f7d9e55585d488b23fe00de11
Jianwei Xie <xiejw0217@gmail.com>
2020-07-25 21:27:10 -0700

    added main.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use mlvm::syntax::ast::Expr;
 use mlvm::syntax::ast::Program;
-use mlvm::syntax::ast::Type;
+use mlvm::syntax::ast::Type::Unknown;
 
 fn main() {
-    let mut p = Program::new(vec![Expr::ArrayLt(
-        Type::Unknown,
-        vec![Expr::new_floatlt(123.)],
-    )]);
+    let mut p = Program::new(vec![
+        Expr::IntLt(Unknown, 23),
+        Expr::ArrayLt(
+            Unknown,
+            vec![Expr::FloatLt(Unknown, 123.), Expr::new_floatlt(7.89)],
+        ),
+    ]);
     print!("before:\n{}", p);
 
     if let Err(err) = p.infer_types() {
