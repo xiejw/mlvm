ceff7f6f897d098fd29ceba83c5e09a243525823
Jianwei Xie <xiejw0217@gmail.com>
2020-07-28 20:22:23 -0700

    added parser.

---
+++ src/syntax/parser.rs
@@ -0,0 +1,21 @@
+use self::super::lexer::Lexer;
+use self::super::token::Token;
+
+pub struct Parser<'a> {
+    lexer: Lexer<'a>,
+    curToken: Box<Token>,
+    peekToken: Box<Token>,
+}
+
+impl Parser<'_> {
+    pub fn new<'a>(input: &'a [u8]) -> Parser<'a> {
+        let mut lexer = Lexer::new(input);
+        let curToken = lexer.next_token();
+        let peekToken = lexer.next_token();
+        Parser {
+            lexer: lexer,
+            curToken: curToken,
+            peekToken: peekToken,
+        }
+    }
+}
