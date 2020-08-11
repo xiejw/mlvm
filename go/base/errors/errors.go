bb4a4023cc8d536d92c5be09cc18fa5331111c89
Jianwei Xie <xiejw0217@gmail.com>
2020-08-11 13:42:41 -0700

    added comments for error.

--- go/base/errors/errors.go
+++ go/base/errors/errors.go
@@ -5,17 +5,21 @@ import (
 	"fmt"
 )
 
+// A special diagnosis error structure, which allows downstream call site to emit more notes after
+// creation.
 type DiagnosisError struct {
 	notes     []string // Reverse order
 	rootCause error
 }
 
+// Creates a DiagnosisError with root cause specified by the message.
 func NewDiagnosisError(sfmt string, args ...interface{}) *DiagnosisError {
 	return &DiagnosisError{
 		rootCause: fmt.Errorf(sfmt, args...),
 	}
 }
 
+// Creates a DiagnosisError with root cause specified by `err` and emit a note immediately..
 func EmitDiagnosisNote(err error, sfmt string, args ...interface{}) *DiagnosisError {
 	de := &DiagnosisError{
 		rootCause: err,
@@ -26,6 +30,7 @@ func EmitDiagnosisNote(err error, sfmt string, args ...interface{}) *DiagnosisEr
 	return de
 }
 
+// Formats the error into string.
 func (de *DiagnosisError) String() string {
 	var buf bytes.Buffer
 
@@ -41,6 +46,7 @@ func (de *DiagnosisError) String() string {
 	return buf.String()
 }
 
+// Emit one more note to the DiagnosisError.
 func (de *DiagnosisError) EmitDiagnosisNote(sfmt string, args ...interface{}) *DiagnosisError {
 	note := fmt.Sprintf(sfmt, args...)
 	de.notes = append(de.notes, note)
