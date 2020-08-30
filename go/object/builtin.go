01f2439fa19e2b82503792c193938e3e180604b9
Jianwei Xie <xiejw0217@gmail.com>
2020-08-30 11:37:30 -0700

    reduce number of lines.

--- go/object/builtin.go
+++ go/object/builtin.go
@@ -6,22 +6,11 @@ type Integer struct {
 	Value int64
 }
 
-func (o *Integer) Type() ObjectType {
-	return IntegerType
-}
-
-func (o *Integer) String() string {
-	return fmt.Sprintf("Integer(%v)", o.Value)
-}
-
 type String struct {
 	Value string
 }
 
-func (o *String) Type() ObjectType {
-	return StringType
-}
-
-func (o *String) String() string {
-	return fmt.Sprintf("String(\"%v\")", o.Value)
-}
+func (o *Integer) Type() ObjectType { return IntegerType }
+func (o *Integer) String() string   { return fmt.Sprintf("Integer(%v)", o.Value) }
+func (o *String) Type() ObjectType  { return StringType }
+func (o *String) String() string    { return fmt.Sprintf("String(\"%v\")", o.Value) }
