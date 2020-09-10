872bacf160fbea876f17219e432a87dc9736e934
Jianwei Xie <xiejw0217@gmail.com>
2020-09-10 07:44:55 -0700

    started doing reduce.

--- go/vm/kernel/reduce.go
+++ go/vm/kernel/reduce.go
@@ -12,5 +12,27 @@ const (
 )
 
 func Reduce(ta *tensorarray.TensorArray, merge_type MergeType) (*tensorarray.TensorArray, *errors.DError) {
+	if merge_type != MergeSum {
+		return nil, errors.New("merge type for reduce is not supported: %v", merge_type)
+	}
+
+	// indices := make([]int, ta.Rank)
+	strides = ta.Strides
+	dims := ta.Dims
+	dim_ptr := ta.Rank -1
+	i := 0
+	buf := ta.Value
+
+	var v float32 = 0.0
+
+	for {
+		v += buf[i]
+		i += strides[dim_ptr]
+
+		if i == dims[dim_ptr] {
+		}
+		break
+	}
+
 	return nil, nil
 }
