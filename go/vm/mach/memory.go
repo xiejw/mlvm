e2c606a287f88098b4ea614bcc6bfa4eea5327e8
Jianwei Xie <xiejw0217@gmail.com>
2020-09-13 14:18:18 -0700

    added size_in_bytes in memory.

--- go/vm/mach/memory.go
+++ go/vm/mach/memory.go
@@ -8,30 +8,33 @@ import (
 const defaultMemoryInitSize = 256
 
 var (
-	errRetrieveFromInvalidSlot = "retrieves an item from in valid slot at: %d (current memory size: %d)."
+	errRetrieveFromInvalidSlot = "retrieves an item from in valid slot at: %d " +
+		"(current allocated slot count: %d)."
 )
 
-// An abstraction of memory to store and load item from slots. If the size is not enough, internally
-// the memory size will be enlarged automatically.
+// An abstraction of memory to store and load item from slots. If allocated slot count is not enough, internally
+// the memory will be enlarged automatically.
 //
 // Internally, we could use a slice or use list of pages.
 type Memory struct {
-	slots []object.Object
-	size  int
+	slots         []object.Object
+	slot_count    int
+	size_in_bytes int
 }
 
 func NewMemory() *Memory {
 	memory := &Memory{
-		slots: make([]object.Object, defaultMemoryInitSize),
-		size:  defaultMemoryInitSize,
+		slots:         make([]object.Object, defaultMemoryInitSize),
+		slot_count:    defaultMemoryInitSize,
+		size_in_bytes: 0,
 	}
 
 	return memory
 }
 
 func (m *Memory) Get(index int) (object.Object, *errors.DError) {
-	if index >= m.size {
-		return nil, errors.New(errRetrieveFromInvalidSlot, index, m.size)
+	if index >= m.slot_count {
+		return nil, errors.New(errRetrieveFromInvalidSlot, index, m.slot_count)
 	}
 
 	item := m.slots[index]
@@ -43,21 +46,29 @@ func (m *Memory) Get(index int) (object.Object, *errors.DError) {
 
 // Deletes the item in memory and returns it.
 func (m *Memory) Drop(index int) (object.Object, *errors.DError) {
-	if index >= m.size {
-		return nil, errors.New(errRetrieveFromInvalidSlot, index, m.size)
+	if index >= m.slot_count {
+		return nil, errors.New(errRetrieveFromInvalidSlot, index, m.slot_count)
 	}
 	item := m.slots[index]
 	if item == nil {
 		return nil, errors.New("the memory slot at %v is empty.", index)
 	}
+	m.size_in_bytes -= item.MemSize()
 	m.slots[index] = nil
 	return item, nil
 }
 
 func (m *Memory) Set(index int, item object.Object) *errors.DError {
-	if index >= m.size {
+	if index >= m.slot_count {
 		panic("Index is too large. Enlarging is planning.")
 	}
+
+	old_item := m.slots[index]
+	if old_item != nil {
+		m.size_in_bytes -= old_item.MemSize()
+	}
+
 	m.slots[index] = item
+	m.size_in_bytes += item.MemSize()
 	return nil
 }
