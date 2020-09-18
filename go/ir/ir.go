d0065043b1a8f5af72de47957cd973f36315912f
Jianwei Xie <xiejw0217@gmail.com>
2020-09-18 13:08:38 -0700

    added ir.

---
+++ go/ir/ir.go
@@ -0,0 +1,55 @@
+package ir
+
+import (
+	"github.com/xiejw/mlvm/go/base/errors"
+)
+
+type Shape struct {
+	Dims []int
+}
+
+type Value interface{}
+
+type TensorValue struct {
+	Shape *Shape
+}
+
+type TupleValue struct {
+}
+
+type Inst interface {
+	GetResult() Value
+	GetResults() []Value
+}
+
+type Fn struct{}
+
+type Builder struct {
+}
+
+func NewBuilder() *Builder {
+	return &Builder{}
+}
+
+func (b *Builder) NewFn(fn_name string) (*Fn, *errors.DError) {
+	return &Fn{}, nil
+}
+
+func (f *Fn) ReadKVStore(key string, s *Shape) Inst {
+	return nil
+}
+
+func (f *Fn) TAdd(lhs, rhs *TensorValue) Inst {
+	return nil
+}
+
+func (f *Fn) MakeTuple(args ...*TensorValue) Inst {
+	return nil
+}
+
+func (f *Fn) SetInput(v Value) Inst {
+	return nil
+}
+
+func (f *Fn) SetOutput(v Value) {
+}
