07cffc1dd6204609582d9017a3449aa4f5bded6a
Jianwei Xie <xiejw0217@gmail.com>
2021-02-18 18:53:13 -0800

    added nn.

---
+++ vm/nn/rng.go
@@ -0,0 +1,32 @@
+package nn
+
+import (
+	"github.com/xiejw/mlvm/vm/mach"
+	"github.com/xiejw/mlvm/vm/object"
+	_ "github.com/xiejw/mlvm/vm/ops"
+)
+
+func RngStdNormal(vm *mach.VM, dtype object.DType, dims []int) *mach.Handle {
+	return nil
+}
+
+func Add(lhs, rhs *mach.Handle) *mach.Handle {
+	return nil
+}
+
+func Mul(lhs, rhs *mach.Handle) *mach.Handle {
+	return nil
+}
+
+func Sum(x *mach.Handle) *mach.Handle {
+	return nil
+}
+
+// func Zeros_(x *mach.Handle) {
+// }
+
+func ZerosGrad(x *mach.Handle) {
+}
+
+func Backward(x *mach.Handle) {
+}
