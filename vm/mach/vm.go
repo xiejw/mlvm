82782fb87111984ab85a045d812c3c8d7c86f1e0
Jianwei Xie <xiejw0217@gmail.com>
2021-02-20 13:05:58 -0800

    started filling execOp.

--- vm/mach/vm.go
+++ vm/mach/vm.go
@@ -1,7 +1,7 @@
 package mach
 
 import (
-	// "github.com/xiejw/mlvm/vm/base/errors"
+	"github.com/xiejw/mlvm/vm/base/errors"
 	"github.com/xiejw/mlvm/vm/object"
 	"github.com/xiejw/mlvm/vm/ops"
 )
@@ -12,19 +12,37 @@ type VM struct {
 }
 
 func (vm *VM) ExecOp(op ops.OpCode, operands []*Handle, opt ops.Option) (*Handle, error) {
-	//	func FillDist(rng Rng, distType DistType, value []float32) {
-	//		switch distType {
-	//		case DistStdNorm:
-	//			StdNorm(rng, value)
-	//			return
-	//		case DistTruncStdNorm:
-	//			TruncStdNorm(rng, value)
-	//			return
-	//		default:
-	//			panic(fmt.Sprintf("unknown distribution type: %v", distType))
-	//		}
-	//	}
-	return nil, nil
+	outputs, err := vm.execOp(op, operands, opt)
+	if err != nil {
+		return nil, err
+	}
+	switch len(outputs) {
+	case 0:
+		return nil, nil
+	case 1:
+		return outputs[0], nil
+	default:
+		return nil, errors.New("expect returning no more than one output for op (%v), but got: %v", op, len(outputs))
+	}
+}
+
+func (vm *VM) execOp(op ops.OpCode, operands []*Handle, opt ops.Option) ([]*Handle, error) {
+	opt = opt.Clone() // make a copy
+	outputs, err := vm.allocateOutputs(op, operands, opt)
+	if err != nil {
+		return nil, errors.WrapNote(err, "failed to allocate outputs during executing op.")
+	}
+
+	err = vm.recordOp(op, operands, outputs, opt)
+	if err != nil {
+		return nil, errors.WrapNote(err, "failed to record during executing op.")
+	}
+
+	err = vm.scheduleOp(op, operands, outputs, opt)
+	if err != nil {
+		return nil, errors.WrapNote(err, "failed to schedule during executing op.")
+	}
+	return outputs, nil
 }
 
 func (vm *VM) NewHandle(dtype object.DType, dims []int) (*Handle, error) {
@@ -39,4 +57,45 @@ func (vm *VM) NewHandle(dtype object.DType, dims []int) (*Handle, error) {
 
 	vm.handles = append(vm.handles, handle)
 	return handle, nil
+}
+
+func (vm *VM) allocateOutputs(op ops.OpCode, operands []*Handle, opt ops.Option) ([]*Handle, error) {
+	switch op {
+	case ops.OP_RNG:
+		return nil, nil
+	default:
+		return nil, errors.New("unsupported op (%v) for allocating outputs", op)
+	}
+}
+
+func (vm *VM) recordOp(op ops.OpCode, operands []*Handle, outputs []*Handle, opt ops.Option) error {
+	// TODO flow grad.
+	vm.tape.Records = append(vm.tape.Records, &Record{
+		Op:       op,
+		Operands: operands,
+		Outputs:  outputs,
+		Option:   opt,
+		FLowGrad: false,
+	})
+	return nil
+}
+
+func (vm *VM) scheduleOp(op ops.OpCode, operands []*Handle, outputs []*Handle, opt ops.Option) error {
+	switch op {
+	//	case ops.OP_RNG:
+	//	func FillDist(rng Rng, distType DistType, value []float32) {
+	//		switch distType {
+	//		case DistStdNorm:
+	//			StdNorm(rng, value)
+	//			return
+	//		case DistTruncStdNorm:
+	//			TruncStdNorm(rng, value)
+	//			return
+	//		default:
+	//			panic(fmt.Sprintf("unknown distribution type: %v", distType))
+	//		}
+	//	}
+	default:
+		return errors.New("unsupported op (%v) for scheduling op", op)
+	}
 }
