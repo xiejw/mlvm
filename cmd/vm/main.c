d12fdcd5f281bd6168c0f2963daa6d61c485e127
Jianwei Xie <xiejw0217@gmail.com>
2021-03-28 18:10:00 -0700

    added main.

--- cmd/vm/main.c
+++ cmd/vm/main.c
@@ -4,6 +4,24 @@
 
 int main()
 {
+        error_t err = OK;
         printf("hello mlvm\n");
-        return 0;
+        struct vm_t*    vm     = vmNew();
+        struct shape_t* r2_2x3 = spNew(2, (int[]){2, 3});
+
+        int t1 = vmNewT(vm, F32, r2_2x3);
+        int t2 = vmNewT(vm, F32, r2_2x3);
+
+        err = vmExec(OP_ADD, NULL, t1, t1, t2);
+        if (err) {
+                errDump("failed to exec op");
+                goto cleanup;
+        }
+
+        printf("succeed.\n");
+
+cleanup:
+        spDecRef(r2_2x3);
+        vmFree(vm);
+        return err;
 }
