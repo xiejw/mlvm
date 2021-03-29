7f6be0a9d94a9d2dfbeef95df46eb0edf6dc2bf1
Jianwei Xie <xiejw0217@gmail.com>
2021-03-28 17:05:02 -0700

    implemented the vm.

--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,29 @@
 #include "vm.h"
 
-void free() {}
+#include "vm_internal.h"
+
+#include <string.h>  // memset
+
+struct vm_t* vmNew()
+{
+        size_t       size = sizeof(struct vm_t);
+        struct vm_t* vm   = malloc(size);
+        memset(vm, 0, size);
+        return vm;
+}
+
+void vmFree(struct vm_t* vm)
+{
+        for (int i = 0; i < MAX_TENSOR_COUNT; i++) {
+                struct tensor_t* t = &vm->handles[i];
+                if (t->used) vmReleaseHandle(t);
+        }
+        free(vm);
+}
+
+error_t vmExec(enum opcode_t op, void* opt, int dst, int lhs, int rhs)
+{
+        return errNew("unimpl for vmExec");
+}
+
+void vmSync() {}
