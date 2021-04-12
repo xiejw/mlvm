d8fb2f7dccbaa9d6342315f57d3f9cb27b62bca1
Jianwei Xie <xiejw0217@gmail.com>
2021-04-11 17:04:45 -0700

    before backprop.

--- cmd/regression/main.c
+++ cmd/regression/main.c
@@ -29,34 +29,46 @@ int main()
         error_t err = OK;
         sds_t   s   = sdsEmpty();
 
+        printf("Linear Regression\n");
+
+        // ---
+        // defines vm with some shapes.
+
         struct vm_t*    vm        = vmNew();
         struct shape_t* sp_weight = spNew(1, (int[]){6});
         struct shape_t* r1_1      = spNew(1, (int[]){1});
         struct opopt_t  opt;
 
+        // ---
+        // prepares the seeds, one for model and one for input.
+
         struct srng64_t* seed           = srng64New(123);
         struct srng64_t* seed_for_input = srng64Split(seed);
         struct srng64_t* rng;  // free after each use.
 
+        // ---
+        // allocates the tensors for model, input and output.
+
+        int w_target = vmTensorNew(vm, F32, sp_weight);
+        int w        = vmTensorNew(vm, F32, sp_weight);
+        int x        = vmTensorNew(vm, F32, sp_weight);
+        int y        = vmTensorNew(vm, F32, r1_1);
+
+        // ---
+        // obtains the pointers to the real data.
+
         float32_t* x_data;
         float32_t* y_data;
         float32_t* w_data;
-
-        printf("Linear Regression\n");
-
-        int w_target = vmTensorNew(vm, F32, sp_weight);
-        int w_learn  = vmTensorNew(vm, F32, sp_weight);
-        int x        = vmTensorNew(vm, F32, sp_weight);
-        int y        = vmTensorNew(vm, F32, r1_1);
 
         NO_ERR(vmTensorData(vm, x, (void**)&x_data));
         NO_ERR(vmTensorData(vm, w_target, (void**)&w_data));
         NO_ERR(vmTensorData(vm, y, (void**)&y_data));
 
         // ---
-        // initial weight for the model (target).
-        opt.mode     = 0;  // normal.
+        // initializes weight for the model (target).
         rng          = srng64Split(seed);
+        opt.mode     = 0;  // normal.
         opt.rng_seed = rng;
         NO_ERR(vmExec(vm, OP_RNG, &opt, w_target, VM_UNUSED, VM_UNUSED));
         srng64Free(rng);
@@ -64,46 +76,54 @@ int main()
         SDS_CAT_PRINTF("\ttarget  weight: ", w_target, "\n");
 
         // ---
-        // initial weight for the model (about to learn).
+        // initializes weight for the model (about to learn).
         rng          = srng64Split(seed);
         opt.rng_seed = rng;
-        NO_ERR(vmExec(vm, OP_RNG, &opt, w_learn, VM_UNUSED, VM_UNUSED));
+        NO_ERR(vmExec(vm, OP_RNG, &opt, w, VM_UNUSED, VM_UNUSED));
         srng64Free(rng);
 
-        SDS_CAT_PRINTF("\tinitial weight: ", w_learn, "\n");
+        SDS_CAT_PRINTF("\tinitial weight: ", w, "\n");
 
-        // --
+        // ---
         // prepare input sample: x,y.
         new_input(seed_for_input, sp_weight->size, x_data, y_data, w_data);
         SDS_CAT_PRINTF("\tinput: ", x, "\n");
         SDS_CAT_PRINTF("\ty: ", y, "\n");
 
-        // formula
+        // --- formula
         //   forward pass
-        //      z = x * w
-        //      rz = reduce_sum(z)
-        //      l = z - y
-        //      l2 = l * l
-        //      loss = reduce_sum(l2)
+        //      z[w] = x[w] * w[w]
+        //      rz[] = reduce_sum(z[w])
+        //      l[] = rz[] - y[]
+        //      l2[] = l[] * l[]
+        //      loss[] = reduce_sum(l2[])
         //   backward pass
-        //      d_l2 = ones with shape as l
-        //      d_l = 2 * l * d_l2
-        //      d_rz = d_l
-        //      dz = ones with shape as l * d_rz
-        //      d_w = d_z * x
+        //      d_l2[] = ones_like(l2[])
+        //      d_l[] = 2 * l[] * d_l2[]
+        //      d_rz[] = d_l[]
+        //      d_z[w] = ones_like(x[w]) * d_rz[]
+        //      d_w[w] = d_z[w] * x[w]
+        //
+        //   backward pass (simplified)
+        //      d_rz[] = 2 * l[]
+        //      d_w[w] = d_rz[] * x[w]
+        //
+        //   optimizer
+        //      d_w[w] = lr[] * d_w[w]  // reuse: update
+        //      w[w] = w[w] - d_w[w]
         //
 
         int z    = vmTensorNew(vm, F32, sp_weight);
         int rz   = vmTensorNew(vm, F32, r1_1);
         int l    = vmTensorNew(vm, F32, r1_1);
         int l2   = vmTensorNew(vm, F32, r1_1);
-        int loss = vmTensorNew(vm, F32, r1_1);
+        int loss = l2;
 
-        NO_ERR(vmExec(vm, OP_MUL, NULL, z, x, w_learn));
+        // forward pass.
+        NO_ERR(vmExec(vm, OP_MUL, NULL, z, x, w));
         NO_ERR(vmExec(vm, OP_REDUCE, &opt, rz, z, VM_UNUSED));
         NO_ERR(vmExec(vm, OP_MINUS, NULL, l, rz, y));
         NO_ERR(vmExec(vm, OP_MUL, NULL, l2, l, l));
-        NO_ERR(vmExec(vm, OP_REDUCE, &opt, loss, l2, VM_UNUSED));
         SDS_CAT_PRINTF("\tloss : ", loss, "\n");
 
         printf("%s\n", s);
