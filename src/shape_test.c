8fb1ac8e19746a4ebefd32765af294a3cffee3f4
Jianwei Xie <xiejw0217@gmail.com>
2021-05-17 13:49:54 -0700

    added ref count check.

--- src/shape_test.c
+++ src/shape_test.c
@@ -5,7 +5,22 @@
 static char*
 test_shape_init()
 {
-        struct shape_t* s = spNew(2, (int[]){2, 3});
+        struct shape_t* s = spNew(2, (int[]){3, 4});
+        ASSERT_TRUE("rank", s->rank == 2);
+        ASSERT_TRUE("dim 0", s->dims[0] == 3);
+        ASSERT_TRUE("dim 1", s->dims[1] == 4);
+        ASSERT_TRUE("size", s->size == 12);
+        spDecRef(s);
+        return NULL;
+}
+
+static char*
+test_shape_ref()
+{
+        struct shape_t* s = spNew(2, (int[]){3, 4});
+        ASSERT_TRUE("ref", s->ref_count == 1);
+        spIncRef(s);
+        ASSERT_TRUE("ref", s->ref_count == 2);
         spDecRef(s);
         return NULL;
 }
@@ -14,5 +29,6 @@ char*
 run_shape_suite()
 {
         RUN_TEST(test_shape_init);
+        RUN_TEST(test_shape_ref);
         return NULL;
 }
