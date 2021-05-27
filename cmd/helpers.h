9efc5fd74ef1273d85b764d8f5e8d94142d938c6
Jianwei Xie <xiejw0217@gmail.com>
2021-05-27 16:45:13 -0700

    move shape macors to vm.h.

--- cmd/helpers.h
+++ cmd/helpers.h
@@ -9,9 +9,6 @@
                 goto cleanup;                                           \
         }
 
-#define R1S(vm, s1)     vmShapeNew(vm, 1, (int[]){(s1)});
-#define R2S(vm, s1, s2) vmShapeNew(vm, 2, (int[]){(s1), (s2)});
-
 #define S_PRINTF(prefix, t, suffix) \
         sdsCatPrintf(&s, prefix);   \
         vmTensorDump(&s, vm, t);    \
--- src/vm.h
+++ src/vm.h
@@ -92,4 +92,8 @@ struct shape_t* spNew(int rank, int* dims);
 struct shape_t* spIncRef(struct shape_t*);
 struct shape_t* spDecRef(struct shape_t*);
 
+#define R1S(vm, s1)     vmShapeNew(vm, 1, (int[]){(s1)});
+#define R2S(vm, s1, s2) vmShapeNew(vm, 2, (int[]){(s1), (s2)});
+
+
 #endif
