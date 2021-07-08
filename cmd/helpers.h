4561cdc70a592ead9f24336ce999ca721344d91d
xiejw.gmac2021 <xiejw0217+gmac2021@gmail.com>
2021-07-08 16:22:55 -0700

    hide one macro.

--- cmd/helpers.h
+++ cmd/helpers.h
@@ -1,8 +1,8 @@
 // Helper methods for cmd/main.go
 
-#define NE(e) NO_ERR_IMPL_(e, __FILE__, __LINE__)
+#define NE(e) _NO_ERR_IMPL_(e, __FILE__, __LINE__)
 
-#define NO_ERR_IMPL_(e, f, l)                                         \
+#define _NO_ERR_IMPL_(e, f, l)                                         \
         if (e) {                                                      \
                 err = e;                                              \
                 errDump("failed to exec op @ file %s line %d", f, l); \
